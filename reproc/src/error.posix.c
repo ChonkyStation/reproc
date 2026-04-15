06034a7fca1ec46eddb4997f7764db89380c5216
xiaozhuai <798047000@qq.com>
2026-04-15 18:17:23 +0800

    Fix build on posix platforms

--- reproc/src/error.posix.c
+++ reproc/src/error.posix.c
@@ -41,4 +41,4 @@ const char *error_string(int error)
     }
     return string;
 #endif
-}                                                                                                                                                      ~            
+}
